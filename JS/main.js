d976574af0502fb7485881882341ab8500076947
Jordan Adams <adamsjt1988@gmail.com>
2018-02-25 21:04:53 -0500

    figured out how to make the image desciptions hidden and only show for an image when it's clicked and then hide again when another image is click. Still need to figure out how to hide the description of the same image is clicked a second time...

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,22 @@
-/* Function for displaying images text */
+/* JS for displaying images text */
 
-$(".click_image").click(function () {
-    $(".hidden").toggle()
+
+//initially hides all divs on images
+$(".hidden").hide();
+
+//brings up image div on click and rehides all other image div
+$(".click_image").on("click", function () {
+    $(".hidden").hide();
+    $(this).siblings(".hidden").toggle();
 })
+
+
+//var description = $(this).siblings(".hidden");
+
+// $(".click_image").on("click", function () {
+//    if (description.is(:hidden)) {
+//         description.show();
+//    }
+//     //$(this).siblings(".hidden").toggle();
+// })
 
