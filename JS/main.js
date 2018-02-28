08e7d86e224e30ffe969c621f17d2bc507fb970a
Jordan Adams <adamsjt1988@gmail.com>
2018-02-27 19:49:19 -0500

    I figured out how to toggle the image captions!

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,23 @@
 /* JS for displaying images text */
 
-
 //initially hides all divs on images
 $(".hidden").hide();
 
 //brings up image div on click and rehides all other image div
-$(".click_image").on("click", function () {
-    $(".hidden").hide();
-    $(this).siblings(".hidden").toggle();
-})
-
-
-//var description = $(this).siblings(".hidden");
-
 // $(".click_image").on("click", function () {
-//    if (description.is(:hidden)) {
-//         description.show();
-//    }
-//     //$(this).siblings(".hidden").toggle();
+//     $(".hidden").hide();
+//     $(this).siblings(".hidden").toggle();
 // })
+
+
+
+
+$(".click_image").on("click", function () {
+   if ($(this).siblings(".hidden").is(":hidden")) {
+    $(".hidden").hide();
+    $(this).siblings(".hidden").show();
+   } else {
+    $(".hidden").hide();
+   }
+})
 
